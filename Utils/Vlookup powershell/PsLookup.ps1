bad99402108e4129b9c350330945b58fe8d3a8eb
BazzoFX <bazzofx@outlook.com>
2023-04-26 11:56:41 +0100

    PsLookup

--- Utils/Vlookup powershell/PsLookup.ps1
+++ Utils/Vlookup powershell/PsLookup.ps1
@@ -1,13 +1,15 @@
-﻿$wd ="C:\Users\Paulo.Bazzo\OneDrive - FitzRoy\Documents\FitzRoy\Github\Powershell Tools\Utils\Vlookup powershell\vlookup data(function)"
+﻿#change for the working path directory of file
 ### Currently Working directory of script
+$wd= "" 
+#-------------- CHANGE ME
 
-$filePath1 = "$wd\info.csv"
-$filePath2 = "$wd\plandayLeave.csv"
-$outLocation = "$wd\Combined.csv"
+$filePath1 = "$wd\main.csv"
+$filePath2 = "$wd\weight.csv"
+$outLocation = "$wd\Oven\Cooked_andCombined_Report.csv"
 $lookupKey = "Salary identifier"
 $column = ""
 
-
+#├----------------------- FUNCTIONS -----------------------
 function Headers1 {
 #$filePath1 = Import-Csv $filePath1
 $filePath1 = Import-Csv $filePath1
@@ -15,14 +17,15 @@ $filePath1 | gm | Where-Object{$_.Name -ne "Equals"} |
                   Where-Object{$_.Name -ne "GetHashCode"} |
                   Where-Object{$_.Name -ne "GetType"} |
                   Where-Object{$_.Name -ne "ToString"} | Select Name, Definition
-}
+} #will show available Heades on file 1
 function Headers2 {
+#$filePath1 = Import-Csv $filePath1
 $filePath2 = Import-Csv $filePath2
 $filePath2 | gm | Where-Object{$_.Name -ne "Equals"} |
                   Where-Object{$_.Name -ne "GetHashCode"} |
                   Where-Object{$_.Name -ne "GetType"} |
                   Where-Object{$_.Name -ne "ToString"} | Select Name, Definition
-}
+} # will show available Heades on file 2
 function pd($filePath1, $filePath2, $column, $lookupKey, $outLocation){
         # Check if Combine.csv exists
     if (Test-Path $outLocation) {
@@ -54,11 +57,17 @@ function pd($filePath1, $filePath2, $column, $lookupKey, $outLocation){
     $filePath1 | Select-Object ($fileHeaders | Where-Object {$_ -ne $column}) |
              Select-Object *, @{name=$column; expression={$hash[$_.$lookupKey]}} |
              Export-Csv $outLocation -NoTypeInformation
-}
-
+} #main function
 function Pslookup ($column) {
 pd -filePath1 $filePath1 -filePath2 $filePath2 -column $column -lookupKey $lookupKey -outLocation $outLocation
-}
+} #main function lazy mode
+#├----------------------- FUNCTIONS -----------------------
 
-Pslookup -column "Balance used"
-Pslookup -column "Available balance"
+#├-----------------------  EXAMPLES 1-----------------------
+Pslookup -column "weight"
+$filePath2 = "$wd\salary.csv"
+Headers2
+Pslookup -column "Salary"
+Pslookup -column "Year"
+#├-----------------------  EXAMPLES 2 -----------------------
+pd -filePath1 $filePath1 -filePath2 $filePath2 -column "Salary" -lookupKey "Salary identifier" -outLocation "$wd/salaryOnly_merged.csv"
