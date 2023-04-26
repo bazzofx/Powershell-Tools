af62e31467a9f178cdc13cf68b493efa8623cd58
BazzoFX <bazzofx@outlook.com>
2023-04-26 11:12:23 +0100

    Write-Emojis fixed

--- Utils/WriteEmojis.ps1
+++ Utils/WriteEmojis.ps1
@@ -1,10 +1,10 @@
 ﻿#From HEX to Io 
 function emoji ($hexValue,$fgColor,$bgColor){
 #Firstly, the code below will convert the Hex value to Integer
-$EmojiIcon = [System.Convert]::toInt32("1F600",16)
+$EmojiIcon = [System.Convert]::toInt32("$hexValue",16)
 #Secondly, convert the Unicode point which is stored in $EmojiIcon to UTF-16 String
  $emoji = Write-Host -ForegroundColor $fgColor -BackgroundColor $bgColor ([System.Char]::ConvertFromUtf32($EmojiIcon)) -NoNewline
  return $emoji
  }
  
- Write-Host "lovely day " -NoNewline; emoji 1F600 Green red
+ Write-Host "lovely day " -NoNewline; emoji 1F600 white red
