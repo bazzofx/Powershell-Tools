b82c9d1b8a497116c94ad69dfe8a8246adde3cbd
BazzoFX <bazzofx@outlook.com>
2023-04-26 10:13:14 +0100

    final Genere-csvHeaders

--- Utils/Generate-CsvHeaders.ps1
+++ Utils/Generate-CsvHeaders.ps1
@@ -1,6 +1,8 @@
 ﻿$stringHeaders = ""
 $string = "First name,Surname,Tax ID,Salary identifier,Department,Leave type,Account,Valid from,Valid until,Bank Holiday calendar,Employee type,Accruing rate value,Accruing rate unit,Period start,Period end,Start balance,Balance at period start,Balance accrued,Balance used,Available balance,Unit,Notes,Leave pay – at the end day of the period filter"
+$separator = ","
 function stringify ($string,$separator) {
+cls
 $blob = $string.split(",")
 $headersCount = $blob.Length - 1
 $count =0
@@ -15,7 +17,22 @@ $count =0
 $count  += 1
     }#cls forEach
 #Write-host $stringHeaders -ForegroundColor Yellow
+    Write-Host "        Would you like to save the result on your clip-board?      " -ForegroundColor White -BackgroundColor blue
+    Write-Host "     TYPE y or yes"  -ForegroundColor White -BackgroundColor blue
+    Write-Host "     TYPE n or no     "  -ForegroundColor White -BackgroundColor blue
+    Write-Host ""
+    $answer = Read-Host "Answer"
+    function yes{Set-Clipboard $stringHeaders}
+
+ switch ($answer){
+        "yes" {yes}
+        "y" {yes}
+        "no" {}
+        default {}
+}
+
+return $stringHeaders
 
 }
 
-stringify $string ","
+stringify -string $string -separator $separator
