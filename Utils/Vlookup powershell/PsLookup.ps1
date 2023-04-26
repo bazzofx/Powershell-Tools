d207745e3691396db4b0cce717caf601eec1aafd
BazzoFX <bazzofx@outlook.com>
2023-04-26 12:07:40 +0100

    This commit is working 100%

--- Utils/Vlookup powershell/PsLookup.ps1
+++ Utils/Vlookup powershell/PsLookup.ps1
@@ -1,15 +1,22 @@
 ﻿#change for the working path directory of file
 ### Currently Working directory of script
-$wd= "" 
+$wd= "C:\Users\Paulo.Bazzo\OneDrive - FitzRoy\Documents\FitzRoy\Github\Powershell Tools\Utils\Vlookup powershell" 
 #-------------- CHANGE ME
 
 $filePath1 = "$wd\main.csv"
 $filePath2 = "$wd\weight.csv"
-$outLocation = "$wd\Oven\Cooked_andCombined_Report.csv"
+$outLocation = "$wd\Oven\Cooked_Report.csv"
 $lookupKey = "Salary identifier"
 $column = ""
 
 #├----------------------- FUNCTIONS -----------------------
+function emoji ($hexValue,$fgColor,$bgColor){
+#Firstly, the code below will convert the Hex value to Integer
+$EmojiIcon = [System.Convert]::toInt32("$hexValue",16)
+#Secondly, convert the Unicode point which is stored in $EmojiIcon to UTF-16 String
+ $emoji = Write-Host -ForegroundColor $fgColor -BackgroundColor $bgColor ([System.Char]::ConvertFromUtf32($EmojiIcon)) -NoNewline
+ return $emoji
+ } 
 function Headers1 {
 #$filePath1 = Import-Csv $filePath1
 $filePath1 = Import-Csv $filePath1
@@ -17,7 +24,7 @@ $filePath1 | gm | Where-Object{$_.Name -ne "Equals"} |
                   Where-Object{$_.Name -ne "GetHashCode"} |
                   Where-Object{$_.Name -ne "GetType"} |
                   Where-Object{$_.Name -ne "ToString"} | Select Name, Definition
-} #will show available Heades on file 1
+}
 function Headers2 {
 #$filePath1 = Import-Csv $filePath1
 $filePath2 = Import-Csv $filePath2
@@ -25,18 +32,28 @@ $filePath2 | gm | Where-Object{$_.Name -ne "Equals"} |
                   Where-Object{$_.Name -ne "GetHashCode"} |
                   Where-Object{$_.Name -ne "GetType"} |
                   Where-Object{$_.Name -ne "ToString"} | Select Name, Definition
-} # will show available Heades on file 2
+}
 function pd($filePath1, $filePath2, $column, $lookupKey, $outLocation){
         # Check if Combine.csv exists
     if (Test-Path $outLocation) {
         # Import Combine.csv if it exists
         $filePath1 = Import-Csv $outLocation
-        Write-Host "         █├♦╩Mergin in progress -- Adding Headers $column to file" -ForegroundColor White -BackgroundColor Blue
+        emoji 1F344 white blue
+        Write-Host " [SUCCESS]      █├♦╬ Mergin in progress -- Adding Header to file name: '$column' " -ForegroundColor White -BackgroundColor Blue
+        
          } 
   else {
         # Import Info.csv if Combine.csv does not exist
         $filePath1 = Import-Csv $filePath1
-        Write-Host " Creating merged file" -ForegroundColor Black -BackgroundColor Yellow
+        emoji 1F525 white red;emoji 1F525 white red;emoji 1F525 white red;emoji 1F525 white red;emoji 1F525 white red;emoji 1F525 white red;emoji 1F525 white red;emoji 1F525 white red
+        Write-Host "   ▀─¦    Turning on the oven..." -ForegroundColor white -BackgroundColor Red -NoNewline
+        emoji 1F525 white red;emoji 1F525 white red;emoji 1F525 white red;emoji 1F525 white red;
+        Start-Sleep 2
+        Write-Host " ►  Adding your files into recipe...   " -ForegroundColor white -BackgroundColor Red
+        Start-Sleep 1
+        #Write-Host "                  █¦╩►  Mixing all ingredients and files together" -ForegroundColor Green
+        emoji 1F344 white blue
+        Write-Host " [SUCCESS]      █├♦╬ Mergin in progress -- Adding Header to file name: '$column' " -ForegroundColor White -BackgroundColor Blue
         }
 
     $filePath2 = Import-Csv $filePath2
@@ -57,17 +74,22 @@ function pd($filePath1, $filePath2, $column, $lookupKey, $outLocation){
     $filePath1 | Select-Object ($fileHeaders | Where-Object {$_ -ne $column}) |
              Select-Object *, @{name=$column; expression={$hash[$_.$lookupKey]}} |
              Export-Csv $outLocation -NoTypeInformation
-} #main function
+}
 function Pslookup ($column) {
 pd -filePath1 $filePath1 -filePath2 $filePath2 -column $column -lookupKey $lookupKey -outLocation $outLocation
-} #main function lazy mode
+}
 #├----------------------- FUNCTIONS -----------------------
 
 #├-----------------------  EXAMPLES 1-----------------------
 Pslookup -column "weight"
 $filePath2 = "$wd\salary.csv"
-Headers2
 Pslookup -column "Salary"
 Pslookup -column "Year"
 #├-----------------------  EXAMPLES 2 -----------------------
-pd -filePath1 $filePath1 -filePath2 $filePath2 -column "Salary" -lookupKey "Salary identifier" -outLocation "$wd/salaryOnly_merged.csv"
+pd -filePath1 $filePath1 -filePath2 $filePath2 -column "Salary" -lookupKey "Salary identifier" -outLocation "$wd\Oven\salaryOnly_merged.csv"
+
+#├-----------------------  EXAMPLES 3 -----------------------
+#Write-Host "Headers for file1"
+#Headers1
+#Write-Host "Headers for file2"
+#Headers2
