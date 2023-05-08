db2075d8ed50eaa345fe2ec5b4d6566c0103e0ef
BazzoFX <bazzofx@outlook.com>
2023-05-08 06:28:26 +0100

    CleanUp Projects

---
+++ Utils/CSV Utils/Create VarNames-based on Headers.ps1
@@ -0,0 +1,5 @@
+﻿$x = "AnnualLeave_Adjust","AnnualLeave_Trent","Available balance","Balance used","ContHours_Trent","DOB_Trent","Email_Trent","Employee type","Entitlement Units:Holiday Entitlement","First name","FirstName_Trent","Gender","Gender_Trent","Hired date","Holiday Year End Date:Holiday Entitlement","Hours per week","Job Title","JobTitle_Trent","PositionStatus_Trent","Postcode_Trent","Primary department","Salary identifier","Service_Trent","StartDate_Trent","Surname","Surname_Trent","Title","Username","FinalBalance_Trent"
+
+ForEach ($word in $x) {
+Write-Host "`$$word = '`$x.$word'"
+}
