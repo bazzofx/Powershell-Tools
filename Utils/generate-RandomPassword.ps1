7df3e9eac8938941af9b77b6c2b4366a7dc7d0a9
BazzoFX <bazzofx@outlook.com>
2023-05-18 16:17:18 +0100

    Added createdUsersBulk

---
+++ Utils/generate-RandomPassword.ps1
@@ -0,0 +1,32 @@
+﻿function generate-randomPassword($times) {
+    function GeneratePassword {
+        $r1 = Get-Random -Maximum 10
+        $r2 = Get-Random -Maximum 10
+        $r3 = Get-Random -Maximum 30
+        $r4 = Get-Random -Maximum 10
+        $r5 = Get-Random -Maximum 30
+        $r6 = Get-Random -Maximum 10
+        $t1 = @("Blue","Red","Green","Silver","Purple","Golden","Pink","Brown","White","Black")
+        $t2 = @("Ocean","Fire","Lake","River","Grass","Bird","Tiger","Fish","Thunder","Car","Hammer")
+        $t3 = @("3","22","33","55","33","55","76","55","23","78","54","23","45","77","88","33","23","99","89","76","55","34","12","34","12","34","23","44","999","888")
+        $t4 = @("UK","BR","CH","US","TW","KK","ZE","AU","PK","RX")
+        $t6 = @("$","#","@","!","%","+","&","%%","@@","##")
+        $pw1 = $t1[$r1]
+        $pw2 = $t2[$r2]
+        $pw3 = $t3[$r3]
+        $pw4 = $t4[$r4]
+        $pw5 = $t3[$r5]
+        $pw6 = $t6[$r6]
+        $tempPass = $pw3 + $pw1 + $pw2 + $pw5 + $pw4 + $pw6
+
+        return $tempPass
+    }
+    if($times -lt 0){$times = 1;}
+    $i = 0
+    while ($i -lt $times) {
+        GeneratePassword
+        $i++
+    }
+}
+
+generate-randomPassword
---
+++ Utils/generateUsersBulk.ps1
@@ -0,0 +1,43 @@
+﻿$wd = "C:\iMsScripts\functions\Create Users Bulk\"
+$path = "C:\iMsScripts\functions\Create Users Bulk\data.csv"
+$data = import-csv $path
+$ou = "OU=Littleover - Staff,OU=East Midlands Support Workers,OU=East Midlands,OU=Offices,DC=efitzroy,DC=org,DC=uk"
+$manager = "Paulo.Bazzo"
+
+
+forEach($x in $data) {
+$serviceName = "Littleover"
+$username = $x.login
+$email ="$username@fitzroy.org"
+$jobTitle = $x."Job Title"
+$pass = $x.Password
+$password = ConvertTo-SecureString -String $pass -AsPlainText -Force
+$displayName = $username.replace("."," ")
+$parts = $username.split(".")
+$name = $parts[0]
+$surname= $parts[1]
+
+    $params = @{
+        SamAccountName = $Username
+        UserPrincipalName = $email
+        Name = $displayName 
+        GivenName = $name 
+        Surname = $surname
+        DisplayName = $displayName
+        AccountPassword = $password
+        Enabled = $true
+        EmailAddress = $email
+        Office = $serviceName
+        Title = $jobTitle
+        Description = $jobTitle
+        Department = $serviceName
+        Company = "FitzRoy"
+        Manager = $manager
+        Path = $ou
+
+    } # -----------------set parameters for each ROW
+
+New-ADUser @params
+Write-Host "[SUCCESS] $email created ----> Password $pass" -ForegroundColor Green
+
+}
