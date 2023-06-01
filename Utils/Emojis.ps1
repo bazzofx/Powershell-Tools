f74e219b64eaddcd4b4f138e11d22f40134f73af
BazzoFX <bazzofx@outlook.com>
2023-06-01 17:48:41 +0100

    emojis udpated

--- Utils/Emojis.ps1
+++ Utils/Emojis.ps1
@@ -5,3 +5,17 @@ $EmojiIcon = [System.Convert]::toInt32("$hexValue",16)
  $emoji = Write-Host -ForegroundColor $fgColor -BackgroundColor $bgColor ([System.Char]::ConvertFromUtf32($EmojiIcon)) -NoNewline
  return $emoji
  } # This adds the emojis
+
+  emoji "1F942" red black
+  emoji "1F37E" white blue
+  emoji "1F967" yellow magenta
+  emoji "1F435" blue white
+  emoji "1F3F4" blue white
+  emoji "1F964" blue white
+  emoji "1F942" blue white
+  emoji "1F37E" blue white
+  emoji "1F967" blue white
+  emoji "1F36A" blue white
+  emoji "1F368" blue white
+  emoji "1F366" blue white
+
