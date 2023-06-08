11df8214bd0cf5e4baf921ba51cb1be7f9864906
BazzoFX <bazzofx@outlook.com>
2023-06-08 19:01:33 +0100

    CSV Bones

---
+++ Utils/CSV Utils/CSV Bones.ps1
@@ -0,0 +1,14 @@
+﻿$array = @()
+
+ForEach($object in $myCsvFile)
+    {
+    $row = New-Object Object
+
+    $row | Add-Member -MemberType NoteProperty -Name "Employee ID" -Value $ID
+    #...
+    #...
+    #...
+
+    $array +=$row
+    }
+    $array | Export-Csv $outLocation -NoTypeInformation
