93296a6972edaa440ffb296f5ea6ff0edd18b70b
BazzoFX <39503563+bazzofx@users.noreply.github.com>
2023-11-22 01:36:43 +0000

    Update SendKeyEvents.ps1

--- Utils/SendKeyEvents.ps1
+++ Utils/SendKeyEvents.ps1
@@ -5,3 +5,23 @@ Start-Sleep 2
 $wsh.SendKeys("{TAB}")
 $wsh.SendKeys({This-is-awesome})
 #Hello[System.Windows.Forms.SendKeys]::SendWait("{TAB}")
+
+
+
+
+# Start Notepad and capture the process object
+#$proc = Start-Process Notepad
+#$wsh.AppActivate("$proc")
+# Start Notepad and capture the process object
+Start-Process Notepad
+# Wait for Notepad to start
+Start-Sleep -Seconds 2
+
+# Activate the Notepad window using the process object
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+[System.Windows.Forms.SendKeys]::SendWait("~Great stuff mate")
+[System.Windows.Forms.SendKeys]::SendWait("{ENTER}")
+[System.Windows.Forms.SendKeys]::SendWait("~Great stuff mate")
+
+
