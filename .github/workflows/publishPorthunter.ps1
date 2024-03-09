213aef2b4d7d588793845ce3bb8791bce44c3ed4
BazzoFX <bazzofx@outlook.com>
2024-03-09 01:49:33 +0000

    updated psd file and yml file

--- .github/workflows/publishPorthunter.ps1
+++ .github/workflows/publishPorthunter.ps1
@@ -1,3 +1,3 @@
 $modulePath = "$PSScriptRoot\porthunter\"
-Write-Host $Env:apiporthunter
+Write-Host "Getting ready to publish it..."
 Publish-Module -path $modulePath -NuGetApiKey $Env:apiporthunter
